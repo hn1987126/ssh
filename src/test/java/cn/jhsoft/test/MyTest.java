b17c3b59e3d8f9f16bae2771ed7eb44101213986
chenyi <chenyi9@jd.com>
2017-08-24 11:03:21 +0800

    test

--- src/test/java/cn/jhsoft/test/MyTest.java
+++ src/test/java/cn/jhsoft/test/MyTest.java
@@ -10,6 +10,12 @@ public class MyTest {
     @Test
     public void test(){
         System.out.println("hhaha");
+
+        String str = "{\"time\":\"1503304821.456\", \"client\":\"111.204.243.6\", \"mac\":18:5e:0f:be:87:a5, \"rssi\":-70, \"lat\":25.555, \"lon\":30.334}";
+        boolean b = str.matches(".*?\\\"mac\\\"\\:(.*?)\\,.*");
+        String str2 = str.replaceAll("(.*?)\\\"mac\\\"\\:(.*?)\\,(.*)", "$1\"mac\":$2$3");
+        System.out.println(str2);
+        System.out.println(b);
     }
 
 }
